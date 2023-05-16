e4ebd9fe7ee3b007a7ef65d10c5d99f2cc91dbe2
Maciech <maciek.chodacki@wp.pl>
2023-05-16 23:33:26 +0200

    "During test phase v1"

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @GetMapping("/greeting")
     public String getGreeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
+        logger.info(name);
         return "Hello, " + name + "!";
     }
 
