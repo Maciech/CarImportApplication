d9ed56c6bdf004107d970f9f5504809db6620dad
Maciech <maciek.chodacki@wp.pl>
2023-05-17 14:47:04 +0200

    During test phase v2

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -3,9 +3,7 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -13,8 +11,21 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String getGreeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
-        logger.info(name);
+        logger.warn(name);
         return "Hello, " + name + "!";
+    }
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register"; // Returns the register.html template
+    }
+
+
+    @PostMapping("/login")
+    public void receiveValueFromFrontend(@RequestBody Login login) {
+        String email = login.getEmail();
+        String password = login.getPassword();
+        // Process the value received from the frontend in your Java backend
+        logger.warn(("Value received from the frontend: " + email + " " + password));
     }
 
 }
